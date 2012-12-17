9efc9b71af77a30fc651541126f42f4a6ac9b4fa
Mykhailo Sakhnik <mykhailo.sakhnik@msakhnik.(none)>
2012-12-17 17:52:58 +0200

    first commit

---
+++ ind.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Hello</title>
+		</head>
+		<body>
+			<h1>Only hello!!!</h1>
+			<p>
+				<?php echo "Hello from PHP!";?>
+				<br /><?php $i = 10; echo $i + 100;?>
+				<br /><?php $i = 10; echo $i + 100;?>
+				<br /><?php echo crypt("123456") ?>
+			</p>
+		</body>
+</html>
